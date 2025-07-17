190159cf10bdfc43dfe71d39949de16e0c1616e1
Rishabhp01 <rishabhpandeyrishu2000@gmail.com>
2025-07-17 08:00:13 +0530

    setup static files of layouts and pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,7 @@
 module.exports.profile = function(req,res){
-    res.end('<h1> User Profile </h1>');
+    res.render('user_profile',{
+        title: "User Profile"
+    });
 }
 module.exports.about = function(req,res){
     res.end('<h1> hello baba </h1>');
