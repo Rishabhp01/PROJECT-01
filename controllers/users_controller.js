07b68e362cdb8e9831ff8b64ab0e1608557e8d47
Rishabhp01 <rishabhpandeyrishu2000@gmail.com>
2025-07-18 22:27:19 +0530

    upto signup page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,8 +1,48 @@
+const User = require('../models/user');
+
+
+
 module.exports.profile = function(req,res){
     res.render('user_profile',{
         title: "User Profile"
     });
 }
-module.exports.about = function(req,res){
-    res.end('<h1> hello baba </h1>');
-}
+ module.exports.signUp = function(req,res){
+    res.render('user_sign_up',{
+        title: "Codeial | Sign Up"
+    })
+ }
+
+ module.exports.signIn = function(req,res){
+    res.render('user_sign_in',{
+        title: "Codeial | Sign In"
+ })
+ }
+
+ // get the sign_up Data
+
+ module.exports.create = function(req,res){
+     if (req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+     }
+
+     User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding user in signing up'); return}
+
+        if (!user){
+            User.create(req.body, function(err,user){
+                        if(err){console.log('error in creating user in signing up'); return}
+
+                        return res.redirect('/users/sign-in');
+            })
+        }else{
+            return res.redirect('back');
+        }
+     })
+ }
+
+// sign in and create the session foe user
+
+ module.exports.createSession = function(req,res){
+//to do later
+ }
